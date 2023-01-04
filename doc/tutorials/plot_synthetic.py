78c2aaa39ed346481984f8a7b3334b0b05af9b01
David Stansby <dstansby@gmail.com>
2023-01-04 14:34:26 +0000

    Clean up tutorial

--- doc/tutorials/plot_synthetic.py
+++ doc/tutorials/plot_synthetic.py
@@ -4,10 +4,8 @@ Calculating a DEM from synthetic data
 This page contains a tutorial, stepping the user through estimating a DEM.
 We don't use any real data here, but instead a series of fake lines that have Gaussian contribution functions.
 """
-import os
-
-os.environ["OMP_NUM_THREADS"] = "1"
-
+##################################################################
+# Start by importing the required modules
 import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,9 +19,10 @@ quantity_support()
 
 ##################################################################
 # To start with we'll create a set of syntemtic emission lines.
-# When these are created we set the center and width of their
-# contribution functions.
+# These are created with their line center and line width, which
+# define their Gaussian contribution functions.
 lines = []
+
 # 20 lines, evenly spaced from 1 MK -> 2 MK
 line_centers = np.linspace(1, 2, 21) * u.MK
 line_width = 0.1 * u.MK
@@ -32,6 +31,7 @@ for line_center in line_centers:
     line = GaussianLine(line_center, line_width)
     lines.append(line)
 
+# 600 temperature bins, linearly spaced from 0.5 -> 2.5 MK
 temp_bins = TempBins(np.linspace(0.5, 2.5, 601) * u.MK)
 
 fig, ax = plt.subplots()
@@ -41,12 +41,12 @@ for line in lines:
 ax.set_title("Line contribution functions")
 
 ##################################################################
-# Now lets create a 'fake' input DEM. We will use this to simulate line
-# intensities that each of these emission lines would observe.
+# Now lets create a 'fake' input DEM. We will use this to simulate
+# the intensities that each of these lines would emit.
 #
-# This input DEM is calculated on a coarse temperature grid. This is
-# because it's easier to infer fewer DEM values using the MCMC inversion
-# later.
+# This input DEM is calculated on a very coarse temperature grid,
+# mainly to make the DEM inversion later much faster for this
+# example.
 coarse_temps = TempBins(np.linspace(1, 2, 6) * u.MK)
 dem_in = np.exp(-(((coarse_temps.bin_centers - 1.2 * u.MK) / (0.2 * u.MK)) ** 2))
 dem_in = BinnedDEM(coarse_temps, dem_in * u.cm**-5)
@@ -60,6 +60,7 @@ ax = axs[1]
 for line in lines:
     ax.stairs(line.get_contribution_function_binned(temp_bins), temp_bins.edges)
 
+ax.set_ylim(bottom=0)
 ax.set_title("Line contribution functions")
 
 ##################################################################
@@ -76,7 +77,8 @@ intensities = u.Quantity([line.intensity_obs for line in lines])
 
 fig, ax = plt.subplots(constrained_layout=True)
 ax.scatter(centers, intensities)
-ax.set_title("Observed line intensities")
+ax.set_title("Simulated line intensities")
+ax.set_xlabel("Line contribution function center / MK")
 
 ##################################################################
 # Now pretend you didn't see the input DEM above! The problem we
@@ -84,8 +86,15 @@ ax.set_title("Observed line intensities")
 #
 #   Given the line contribution functions, and the observed
 #   intensity in each line, what was the original DEM?
+#
+# To do this we use the ``predict_dem_emcee`` function to
+# do a DEM inversion. This takes the list of lines we created
+# earlier, and the temperature bins that we want to estimate
+# the DEM in.
 if __name__ == "__main__":
+    # Run DEM inversion
     sampler = predict_dem_emcee(lines, dem_in.temp_bins, nsteps=1000)
+    # Get results
     samples = sampler.get_chain()
 
     fig, ax = plt.subplots()
